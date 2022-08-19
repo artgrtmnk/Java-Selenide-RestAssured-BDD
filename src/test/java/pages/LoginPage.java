c2976b34865634ca4ae4a0f243fe3e789e99a12f
artgrtmnk <78636533+artgrtmnk@users.noreply.github.com>
2022-08-19 16:41:08 +0400

    Can you work stable, please?

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -9,20 +9,20 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
-    public SelenideElement userNameField = $("label[for='sso_username']");
+    public SelenideElement userNameField = $("#sso_username");
     public SelenideElement passwordField = $("#ssopassword");
     public SelenideElement submitButton = $("#signin_button");
     public SelenideElement errorMessage = $("#errormsg");
 
     public void openPage() {
         Selenide.open("https://profile.oracle.com/");
-        this.userNameField.should(exist);
-        this.passwordField.should(exist);
-        this.submitButton.should(exist);
+        //this.userNameField.shouldBe(visible);
+        this.passwordField.shouldBe(visible);
+        this.submitButton.shouldBe(visible);
     }
 
     public void enterUserName(String username) {
-        this.userNameField.shouldBe(visible).val(username);
+        this.userNameField.val(username);
     }
 
     public void enterPassword(String password) {
